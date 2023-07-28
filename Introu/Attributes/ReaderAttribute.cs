166d7f29212c576e70192417fa57f5d9c3a145cc
spocot <spencerkocot@gmail.com>
2023-07-28 13:55:03 -0400

    Add Reader and Writer attributes

---
+++ Introu/Attributes/ReaderAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introu.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class ReaderAttribute : Attribute { }
+}
